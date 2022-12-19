d384578db9b981be9bcb37f96e4e2ecdebf39bed
Allassane <dickoallassane1997@gmail.com>
2022-12-19 16:18:22 +0100

    Add sorting list of score: so the highest score are at the top

--- src/modules/ScoreManager.js
+++ src/modules/ScoreManager.js
@@ -5,17 +5,38 @@ export default class ScoreManager {
   }
 
   add = (name, value) => {
-    this.list.push({'value': value, 'name': name})
-    this.display({'value': value, 'name': name})
+    const score = {'value': value, 'name': name}
+
+    this.list.push(score)
+
+    this.list = this.list.sort((a, b) => b.value - a.value)
+    const index  = this.list.indexOf(score)
+
+    this.display(score, index)
   } 
 
-  display = (score) => {
+  display = (score, index=this.list.length) => {
     const liScore = document.createElement('li')
     liScore.classList.add('score')
     liScore.textContent = `${score.name}: ${score.value}`
 
     const ulListScore = document.getElementById('list-scores');
-    ulListScore.append(liScore)
+
+    if(index===this.list.length){
+      ulListScore.append(liScore)
+      return
+    }
+
+    //Order the list: Insert current Score at index (after the higher score)
+    let currentIndex = index
+    let currentli = ulListScore.firstChild
+    while (currentIndex!=0){
+      currentli = currentli.nextSibling
+      currentIndex -= 1
+    }
+    ulListScore.insertBefore(liScore, currentli)
+    //insertAfter(currentli, liScore)
+    console.log(this.list)
   }
 
 }
